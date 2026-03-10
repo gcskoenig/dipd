27dd7002c58f03c0d7c4b9669d37d9bff78e8fed
gcskoenig <gunnar.koenig@pm.me>
2026-03-10 15:16:09 +0100

    Remove empty test stubs, unused constants, and commented-out code; export learners from __init__

--- src/dipd/__init__.py
+++ src/dipd/__init__.py
@@ -1,2 +1,5 @@
 from dipd.explainer import DIP
+from dipd.learners import EBM, LinearGAM
 from dipd.consts import RETURN_NAMES
+
+__all__ = ["DIP", "EBM", "LinearGAM", "RETURN_NAMES"]
--- src/dipd/consts.py
+++ src/dipd/consts.py
@@ -1,14 +1,6 @@
 RETURN_NAMES = ['v1', 'v2', 'vC', 'main_effect_cross_predictability', 'main_effect_cov', 'pure_interactions']
-# FORCEPLOT_COLOR_DICT = {'main_effect_dependencies': '#9F76F5', 'main_effect_cross_predictability': '#C776F5',
-#                         'main_effect_cov': '#7677F5', 'pure_interactions': '#7AF58D',
-#                         'v2': 'gray', 'v1': 'darkgrey', 'total': 'black',
-#                         'vC': 'lightgrey'}
 
 FORCEPLOT_COLOR_DICT = {'main_effect_dependencies': '#946EE6', 'main_effect_cross_predictability': '#C862CC',
                         'main_effect_cov': '#6285CC', 'pure_interactions': '#77E69E',
                         'v2': 'gray', 'v1': 'darkgrey', 'total': 'black',
                         'vC': 'lightgrey', 'loco': '#D98068', 'joint': '#A69C37'}
-
-PLOTS_FONT_AESTHETICS = {'fontsize': 7, 'fontname': 'Arial'}
-
-# NOTE: pure interaction color for text #64C285
